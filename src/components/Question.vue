f34bebb94589d7c4b424e6fd2fa5548fda2a7b69
Henry Lin <henry.ys.lin@gmail.com>
2021-01-24 14:33:38 +0800

    Center question text

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <b>{{ questionText }}</b>
+  <div class="outer">
+    <div class="inner">
+      <b>{{ questionText }}</b>
+    </div>
   </div>
 </template>
 
@@ -15,14 +17,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-div {
+.outer {
   overflow-wrap: break-word;
   font-size: 20px;
   text-overflow: ellipsis;
   overflow: hidden;
+  position: relative;
+  height: 100%;
 }
-div b {
-  display: block;
+.inner {
   margin: 10px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
 }
 </style>
