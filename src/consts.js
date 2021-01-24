37ff627cdeda6d23b136fa67546ae611fbc05422
Henry Lin <henry.ys.lin@gmail.com>
2021-01-24 13:51:24 +0800

    Question prompt

--- src/consts.js
+++ src/consts.js
@@ -1,8 +1,6 @@
 export default {
-  questions: [
-    "Click something!",
-    "",
-    "Give me some response!",
-    ""
-  ]
-}
+  questions: ["Click something!", "Give me some response!"],
+  randomQuestion() {
+    return this.questions[Math.floor(Math.random() * this.questions.length)];
+  },
+};
